545ca5297a08a9caaadcbab72f767f6a1cc00ec5
Sarah Scheffler <sscheffler@hmc.edu>
2017-04-11 13:29:54 -0400

    Version 1.1 of encryption reference.  It now all works, though the key management isn't pretty.

--- encryption_ref.js
+++ encryption_ref.js
@@ -17,7 +17,7 @@ var assert = require('assert');
 // File ID and file key
 var fileID = 1;
 var file = Buffer.from("This is a file buffer");
-var fileKey = Buffer.allocUnsafe(sodium.crypto_aead_256gcm_KEYBYTES);
+var fileKey = Buffer.allocUnsafe(sodium.crypto_aead_aes256gcm_KEYBYTES);
 sodium.randombytes_buf(fileKey);
 
 // Users and their keys
@@ -25,8 +25,9 @@ sodium.randombytes_buf(fileKey);
 //TODO: this should be GCM_SIV, not plain GCM.
 var userKeys = {};
 var patient1 = "abcd efg"
-var patient1Key = Buffer.allocUnsafe(sodium.crypto_aead_256gcm_KEYBYTES);
+var patient1Key = Buffer.allocUnsafe(sodium.crypto_aead_aes256gcm_KEYBYTES);
 sodium.randombytes_buf(patient1Key);
+userKeys[patient1] = patient1Key;
 var doctor1 = "hijkl mnopqr"
 var doctor1Key = Buffer.allocUnsafe(sodium.crypto_aead_aes256gcm_KEYBYTES);
 sodium.randombytes_buf(doctor1Key);
@@ -40,17 +41,18 @@ userKeys[doctor1] = doctor1Key;
 var encryptedFileKeys = {};
 var fileKeyPatient1Metadata = Buffer.from(patient1 + "|" + fileID);
 var nonce1 = Buffer.allocUnsafe(sodium.crypto_aead_aes256gcm_NPUBBYTES);
+encryptedFileKeys[fileID] = {};
 encryptedFileKeys[fileID][patient1] = sodium.crypto_aead_aes256gcm_encrypt(fileKey,
 	fileKeyPatient1Metadata, nonce1, userKeys[patient1]);
 var fileKeyDoctor1Metadata = Buffer.from(doctor1 + "|" + fileID);
 var nonce2 = Buffer.allocUnsafe(sodium.crypto_aead_aes256gcm_NPUBBYTES);
 encryptedFileKeys[fileID][doctor1] = sodium.crypto_aead_aes256gcm_encrypt(fileKey,
-	fileKeyDoctor1metadata, nonce2, userKeys[doctor1]);
+	fileKeyDoctor1Metadata, nonce2, userKeys[doctor1]);
 
 // Encrypt file under file key, and include the encrypted file keys as metadata
 var fileMetadata = Buffer.from(fileID+"|||||"+
 	patient1+"|"+encryptedFileKeys[fileID][patient1]+"|||"+
-	doctor1+"|"+encryptedFileKeys[fileID][doctor1]"|||");
+	doctor1+"|"+encryptedFileKeys[fileID][doctor1]+"|||");
 var nonce = Buffer.allocUnsafe(sodium.crypto_aead_aes256gcm_NPUBBYTES);
 sodium.randombytes_buf(nonce);
 var fileCiphertext = sodium.crypto_aead_aes256gcm_encrypt(file, fileMetadata,
@@ -69,15 +71,15 @@ var fileCiphertext = sodium.crypto_aead_aes256gcm_encrypt(file, fileMetadata,
 // not from some dictionary. Though then we still have to store the nonce there?
 patient1Key = patient1Key;
 var patient1FileKey = sodium.crypto_aead_aes256gcm_decrypt(
-	encryptedFileKeys[fileId][patient1], fileKeyPatient1Metadata, nonce1, patient1Key);
+	encryptedFileKeys[fileID][patient1], fileKeyPatient1Metadata, nonce1, patient1Key);
 var filePlaintext = sodium.crypto_aead_aes256gcm_decrypt(
-	fileCiphertext, fileMetadata, nonce, patient1FileKey)
+	fileCiphertext, fileMetadata, nonce, patient1FileKey);
 
-if (typeof plaintext == 'undefined') {
+if (typeof filePlaintext == 'undefined') {
 	console.log("Decryption failed");
 } else {
 	console.log("Decryption succeeded!");
-	console.log(plaintext.toString());
+	console.log(filePlaintext.toString());
 }
 
 
